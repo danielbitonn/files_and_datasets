d994475ff556dd138ce8aa64b02e863bdd19b297
Daniel Biton <47026541+danielbitonn@users.noreply.github.com>
2023-02-01 15:30:51 +0200

    Update _import_alias.py

--- utilities/_import_alias.py
+++ utilities/_import_alias.py
@@ -1,7 +1,9 @@
+%load_ext autotime
+%load_ext watermark
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sweetviz as sv
 import os
-%load_ext autotime
-%load_ext watermark
+import yaml
